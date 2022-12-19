dead95ebda56eef1d652bb5e048a665208ea1041
asif10388 <asif.khan10388@gmail.com>
2022-12-19 20:51:11 +0600

    new features

--- client.js
+++ client.js
@@ -1,79 +1,136 @@
-const net = require('net');
-const { WebSocket } = require('ws');
+const net = require("net");
+const { WebSocket } = require("ws");
+const zero = require("dev-zero-stream");
 
 let serverAddress = process.env.HOST;
 
+const parameters = process.argv.slice(2);
+
+let upload = false;
+let download = false;
+
+let downloadMbpsToSend = 0;
+
+if (parameters.length > 0) {
+  parameters.find((param) => {
+    if (param.includes("-D")) {
+      download = true;
+      console.log("Download test");
+    } else if (param.includes("-U")) {
+      upload = true;
+      console.log("Upload test");
+    }
+  });
+}
+
 function start() {
-    console.log(`SERVER: ${serverAddress}`);
+  console.log(`SERVER: ${serverAddress}`);
 
-    const ws = new WebSocket(`ws://${serverAddress}:9075/`);
+  const ws = new WebSocket(`ws://${serverAddress}:9075/`);
+  const client = new net.Socket();
+  client.connect(9074, serverAddress);
 
-    let pbi = 0;
-    const pingBuffer = Array(5).fill(0);
-    const totalWeight = (pingBuffer.length / 2) * ((2 * pingBuffer.length) + (pingBuffer.length - 1) * -1);
+  let pbi = 0;
 
-    let lastPingTime = 0;
-    
-    function sendPing() {
-        lastPingTime = Date.now();
-        ws.send('I');
-    }
+  const pingBuffer = Array(5).fill(0);
 
-    ws.on('open', () => {
-        sendPing();
+  const totalWeight =
+    (pingBuffer.length / 2) *
+    (2 * pingBuffer.length + (pingBuffer.length - 1) * -1);
+
+  let lastPingTime = 0;
+
+  function sendPing() {
+    lastPingTime = Date.now();
+    ws.send("I");
+  }
+
+  ws.on("open", () => {
+    sendPing();
+  });
+
+  function runUploadTest() {
+    let sinceLastUpload = 0;
+
+    const stream = zero();
+
+    stream.on("data", (data) => {
+      sinceLastUpload += data.length;
     });
 
-    function runSpeedTest() {
-        let last = Date.now();
-        let sinceLast = 0;
+    stream.pipe(client);
+  }
 
-        const client = new net.Socket();
-        client.connect(9074, serverAddress);
+  if (upload) {
+    runUploadTest();
+  } else {
+    console.log("No upload test issued");
+  }
 
-        client.on('data', data => {
-            sinceLast += data.length;
-        });
+  function runDownloadSpeedTest() {
+    let last = Date.now();
+    let sinceLastDownload = 0;
 
-        setInterval(() => {
-            const sinceLastMB = (sinceLast * 8)/(1024 * 1024);
-
-            const diff = (Date.now() - last) / 1000;
-            const mbps = Math.round((sinceLastMB / diff) * 100) / 100;
-
-            console.log(`${mbps} Mb/s`);
-            sinceLast = 0;
-
-            last = Date.now();
-        }, 1000);
-    }
-
-    runSpeedTest();
-
-    ws.on('message', (message) => {
-        const msg = message.toString();
-        
-        if (msg[0] === 'O') {
-            const diff = Date.now() - lastPingTime;
-            pingBuffer[pbi] = diff / 3;
-
-            let sum = 0;
-
-            for (let i = 0; i < pingBuffer.length; i++) {
-                const idx = (pbi + (i * -1) + pingBuffer.length) % pingBuffer.length;
-                const pingValue = pingBuffer[idx] * (pingBuffer.length - i);
-
-                sum += pingValue;
-            }
-
-            const averagePing = sum/totalWeight;
-            ws.send(`P:${Math.round(averagePing)}`);
-
-            console.log(`${Math.round(averagePing)} ms`);
-
-            pbi = (pbi + 1) % pingBuffer.length;
-            setTimeout(sendPing, 500);
-        }
+    client.on("data", (data) => {
+      sinceLastDownload += data.length;
     });
+
+    client.setMaxListeners(0);
+
+    setInterval(() => {
+      const sinceLastDownloadMB = (sinceLastDownload * 8) / (1024 * 1024);
+      const diff = (Date.now() - last) / 1000;
+
+      const downloadMbps = Math.round((sinceLastDownloadMB / diff) * 100) / 100;
+
+      downloadMbpsToSend = downloadMbps;
+
+      console.log(`Download rate: ${downloadMbps} Mb/s`);
+
+      sinceLastDownload = 0;
+
+      last = Date.now();
+    }, 1000);
+  }
+
+  if (download) {
+    runDownloadSpeedTest();
+  } else {
+    console.log("No download test issued");
+  }
+
+  ws.on("message", (message) => {
+    const msg = message.toString();
+
+    if (msg[0] === "O") {
+      const diff = Date.now() - lastPingTime;
+      pingBuffer[pbi] = diff / 3;
+
+      let sum = 0;
+
+      for (let i = 0; i < pingBuffer.length; i++) {
+        const idx = (pbi + i * -1 + pingBuffer.length) % pingBuffer.length;
+        const pingValue = pingBuffer[idx] * (pingBuffer.length - i);
+
+        sum += pingValue;
+      }
+
+      const averagePing = sum / totalWeight;
+      const d = downloadMbpsToSend;
+      ws.send(`D ${d} ${averagePing}`);
+
+      // ws.send(
+      //   `P:${Math.round(
+      //     averagePing
+      //   )} ms ${downloadMbpsToSend} ${uploadMbpsToSend}`
+      // );
+
+      console.log(`${Math.round(averagePing)} ms`, downloadMbpsToSend);
+
+      pbi = (pbi + 1) % pingBuffer.length;
+      setTimeout(sendPing, 500);
+    }
+  });
 }
 
 start();
--- server.js
+++ server.js
@@ -1,54 +1,139 @@
-const os = require('os');
-const ws = require('ws');
-const net = require('net');
-const zero = require('dev-zero-stream');
+const os = require("os");
+const ws = require("ws");
+const net = require("net");
+const path = require("path");
+const axios = require("axios");
+const zero = require("dev-zero-stream");
+const bodyParser = require("body-parser");
 
-const interfaces = os.networkInterfaces()
+const interfaces = os.networkInterfaces();
 
-const ips = Object
-    .values(interfaces)
-    .flat()
-    .filter(inf => inf.family === 'IPv4')
-    .map(inf => {
-        return inf.address
-    });
+const ips = Object.values(interfaces)
+  .flat()
+  .filter((inf) => inf.family === "IPv4")
+  .map((inf) => {
+    return inf.address;
+  });
 
 const wss = new ws.WebSocketServer({
-    port: 9075
+  port: 9075,
 });
 
+let clients = {};
+let downloadMbps = 0;
+let uploadMbps = 0;
+let ping = 0;
 
-wss.on('connection', ws => {
-    ws.on('message', message => {
-        const str = message.toString();
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    const str = message.toString();
 
-        if (str[0] === 'I') {
-            ws.send('O');
-        } else if (str[0] === 'P') {
-            // ws.send('O');
-        } else if (str[0] === 'D') {
-            // ws.send('O');
-        }
-    });
+    const [_, d, p] = str.split(" ");
+
+    if (d !== undefined && p !== undefined) {
+      downloadMbps = parseFloat(d);
+      ping = parseInt(p, 10);
+    }
+
+    if (str[0] === "I") {
+      ws.send("O");
+    } else if (str[0] === "P") {
+    } else if (str[0] === "U") {
+    }
+  });
+
+  ws.on("close", () => {
+    ping = 0;
+    downloadMbps = 0;
+  });
 });
 
 let server = net.createServer();
 
-server.on('connection', (connection) => {
-    const z = zero();
-    z.pipe(connection);
+let last = Date.now();
+let sinceLast = 0;
 
-    connection.on('error', () => {});
+server.on("connection", (connection) => {
+  connection.on("error", () => console.log("Connection error"));
+
+  const id = Date.now();
+
+  const z = zero();
+  z.pipe(connection);
+
+  connection.on("data", (data) => {
+    sinceLast += data.length;
+  });
+
+  setInterval(() => {
+    const sinceLastMB = (sinceLast * 8) / (1024 * 1024);
+
+    const diff = (Date.now() - last) / 1000;
+    const mbps = Math.round((sinceLastMB / diff) * 100) / 100;
+
+    if (uploadMbps !== undefined) {
+      uploadMbps = mbps;
+    }
+
+    clients[id] = {
+      id: id,
+      ping: ping,
+      uploadRate: uploadMbps,
+      downloadRate: downloadMbps,
+      address: connection.remoteAddress,
+    };
+
+    axios
+      .post("http://localhost:9073/data", {
+        clients: clients,
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    console.log(clients);
+    // console.log(`Upload rate: ${mbps} Mb/s`);
+
+    sinceLast = 0;
+    last = Date.now();
+  }, 1000);
 });
 
-server.listen(9074, '0.0.0.0');
+server.listen(9074, "0.0.0.0");
 
-const fs = require('fs/promises');
-const app = require('express')();
+const fs = require("fs/promises");
+const app = require("express")();
 
-app.get('/termux', async (req, res) => {
-    const content = (await fs.readFile('./termux.sh')).toString('utf-8');
-    res.end(content.replace('999.999.999.999', req.headers.host.split(':').shift()));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(9073, '0.0.0.0');
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname + "/table.html"));
+});
+
+app.get("/termux", async (req, res) => {
+  const content = (await fs.readFile("./termux.sh")).toString("utf-8");
+  res.end(
+    content.replace("999.999.999.999", req.headers.host.split(":").shift())
+  );
+});
+
+app.post("/data", (req, res) => {
+  clients = req.body.clients;
+  res.status(200);
+});
+
+app.get("/data", (req, res) => {
+  res.send({
+    clients: clients,
+  });
+});
+
+app.listen(9073, "0.0.0.0", () => console.log("Server started"));
